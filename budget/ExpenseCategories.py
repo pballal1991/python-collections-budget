27db0c1cd9dba3c2ad6228544ec4c5af2a0f91e3
Prathibha Ballal <prathibha@Prathibhas-MacBook-Pro.local>
2021-05-24 15:22:55 -0700

    category updated

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,10 +1,48 @@
-from . import Expense
+import Expense
+import timeit
 import matplotlib.pyplot as plt
+
 
 def main():
     expenses = Expense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
+    expenses.read_expenses('../data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+    print(divided_set_comp)
+    if divided_set_comp != divided_for_loop:
+        print('Sets are NOT equal by == test')
+
+    for a, b in zip(divided_for_loop, divided_set_comp):
+        if a.issubset(b) != b.issubset(a):
+            print('Sets are NOT equal by subset test')
+    timeit.timeit(
+        stmt="expenses.categorize_for_loop()",
+        setup='''import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('../data/spending_data.csv')''',
+        number=100000,
+        globals=globals())
+    print(timeit.timeit())
+
+    timeit.timeit(
+        stmt="expenses.categorize_set_comprehension()",
+        setup='''import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('../data/spending_data.csv')''',
+        number=100000,
+        globals=globals())
+    print(timeit.timeit())
+
+    fig, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+    divided_expenses_sum = []
+
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
+
+    plt.show()
 
 
 if __name__ == "__main__":
